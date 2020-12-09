425f19c22d247c0a978427a83ea583152ddf5bc4
Antares <antares.oier@gmail.com>
2020-12-10 04:20:36 +0800

    change struct

--- src/api.js
+++ src/api.js
@@ -95,3 +95,17 @@ app.post("/delete_message", async (req, res) => {
   }
 });
 
+app.get("/get_members", async (req, res) => {
+  try {
+    let members = await bot.getGroupMemberList();
+    res.send({
+      code: 200,
+      msg: "ok",
+      members
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({code: 500, msg: e.toString()});
+  }
+});
+
